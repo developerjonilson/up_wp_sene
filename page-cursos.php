bc842ddd7162885bad65414fe3a599da965528c7
jonilsondeveloper <jonilsondeveloper@gmail.com>
2018-04-09 21:55:32 -0300

    cursos em andamento

---
+++ page-cursos.php
@@ -0,0 +1,19 @@
+<?php
+// template name: cursos
+get_header();
+ ?>
+
+<section class="container custom-container">
+  <br>
+
+  <div class="row">
+    
+  </div>
+
+
+  <br>
+</section>
+
+ <?php
+ get_footer();
+  ?>
