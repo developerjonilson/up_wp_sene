bc111fecd875889af4b08c9ab9c6ee2863770aeb
jonilsondeveloper <jonilsondeveloper@gmail.com>
2018-04-09 21:51:37 -0300

    atendimento concluido

--- page-atendimento.php
+++ page-atendimento.php
@@ -1,32 +1,36 @@
-<?php 
+<?php
     // template name: atendimento
 
-    get_header(); 
+    get_header();
 ?>
+    <section class="container custom-container">
+      <br>
+      <h3 class="text-center"> <?php the_title(); ?> </h3>
+      <br>
 
-    <section class="container">
-    
+      <form action="#" method="post">
+          <div class="form-group">
+              <label for="name">Nome</label>
+              <input type="text" name="name" id="name" class="form-control">
+          </div>
+          <div class="form-group">
+              <label for="name">E-mail</label>
+              <input type="text" name="email" id="email" class="form-control">
+          </div>
+          <div class="form-group">
+              <label for="name">Assunto</label>
+              <input type="text" name="assunto" id="mensagem" class="form-control">
+          </div>
+          <div class="form-group">
+              <label for="name">Mensagem</label>
+              <textarea name="mensagem" id="mensagem" cols="30" rows="5" class="form-control"></textarea>
+          </div>
+          <div class="form-group">
+              <input type="submit" class="btn btn-primary" value="Enviar Mensagem">
+          </div>
+      </form>
+
+      <br><br>
     </section>
-    <form action="#">
-        <div class="form-group">
-            <label for="name">Nome</label>
-            <input type="text" name="name" id="name" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="name">E-mail</label>
-            <input type="text" name="email" id="email" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="name">Assunto</label>
-            <input type="text" name="assunto" id="mensagem" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="name">Mensagem</label>
-            <textarea name="mensagem" id="mensagem" cols="30" rows="10" class="form-control"></textarea>
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-success" value="Enviar">
-        </div>
-    </form>
 
 <?php get_footer(); ?>
